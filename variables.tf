20d580ac5e84f4ac0fbc6423962f11178c60426f
Delcio Agostinho <dagostinho4@gmail.com>
2025-07-31 23:44:00 +0100

    created secority group

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "region" {
+  description = "AWS region to launch servers."
+  default     = "eu-west-1"
+}
